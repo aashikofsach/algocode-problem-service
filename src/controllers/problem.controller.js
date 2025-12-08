b0c40a693dfe9b691afa5859d0b9406486346b63
aashish <ritikkamboj4314@gmail.com>
2025-12-08 20:46:09 +0530

    Feat : Added Router Structure for versioned api for problems , having issue

---
+++ src/controllers/problem.controller.js
@@ -0,0 +1,35 @@
+function pingProblemController(req, res)
+{
+    return res.json({message : "Ping check is ok for problems controller"})
+}
+
+function addProblem(req, res)
+{
+
+}
+
+function getProblems(req, res)
+{
+
+}
+function getProblem(req, res)
+{
+
+}
+function updateProblem(req, res)
+{
+
+}
+function deleteProblem(req, res)
+{
+
+}
+
+module.exports ={
+    addProblem,
+    getProblem, 
+    getProblems,
+    updateProblem,
+    deleteProblem,
+    pingProblemController
+}
--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const {PORT}= require('./config/server.config');
+const apiRouter = require('./routes');
 
 
 const app = express() ;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.text);
+app.use(bodyParser.text());
 
-
+app.get('/api', apiRouter)
 
 app.get("/ping", (req, res)=>
 {
