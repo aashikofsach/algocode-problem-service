94df5aae2b9d2d96a6e8f3f0018697f7d51989fb
aashish <ritikkamboj4314@gmail.com>
2025-12-10 19:47:41 +0530

    Fix : migrated from numeric status codes to http-status-codes library

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,22 +1,34 @@
+const { StatusCodes } = require("http-status-codes");
+
 function pingProblemController(req, res) {
   return res.json({ message: "Ping check is ok for problems controller" });
 }
 
 function addProblem(req, res) {
-  return res.status(501).json({ message: "Not implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not implemented" });
 }
 
 function getProblems(req, res) {
-  return res.status(501).json({ message: "Not implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not implemented" });
 }
 function getProblem(req, res) {
-  return res.status(501).json({ message: "Not implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not implemented" });
 }
 function updateProblem(req, res) {
-  return res.status(501).json({ message: "Not implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not implemented" });
 }
 function deleteProblem(req, res) {
-  return res.status(501).json({ message: "Not implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not implemented" });
 }
 
 module.exports = {
